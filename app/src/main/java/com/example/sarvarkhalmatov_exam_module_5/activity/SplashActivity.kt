7149e720971572565d3a7452902c46c18332d3a5
Developer <Jaxadev>
2022-02-25 15:10:48 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sarvarkhalmatov_exam_module_5/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sarvarkhalmatov_exam_module_5.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sarvarkhalmatov_exam_module_5.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
